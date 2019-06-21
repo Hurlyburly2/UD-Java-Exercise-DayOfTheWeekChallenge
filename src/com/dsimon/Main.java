999520c597fc906496f896115df41b2c6b1b2f14
Hurlyburly2 <dougdougmann@gmail.com>
2019-06-20 20:30:11 -0400

    exercise complete

--- src/com/dsimon/Main.java
+++ src/com/dsimon/Main.java
@@ -3,6 +3,14 @@ package com.dsimon;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        DayOfTheWeek.printDayOfTheWeek(-1);
+        DayOfTheWeek.printDayOfTheWeek(0);
+        DayOfTheWeek.printDayOfTheWeek(1);
+        DayOfTheWeek.printDayOfTheWeek(2);
+        DayOfTheWeek.printDayOfTheWeek(3);
+        DayOfTheWeek.printDayOfTheWeek(4);
+        DayOfTheWeek.printDayOfTheWeek(5);
+        DayOfTheWeek.printDayOfTheWeek(6);
+        DayOfTheWeek.printDayOfTheWeek(7);
     }
 }
